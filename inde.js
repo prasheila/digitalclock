2ab3fb3737b47e16d7e72b2409ccec74457c32d3
prasheila <81790655+prasheila@users.noreply.github.com>
2023-03-10 21:29:29 +0530

    done

---
+++ inde.js
@@ -0,0 +1,30 @@
+const hourE= document.getElementById("hour");
+const minuteE=document.getElementById("minutes");
+const secondE=document.getElementById("seconds");
+const ampmE=document.getElementById("ampm");
+
+function upclock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s= new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h>12){
+        h = h-12;
+        ampm = "PM";
+    }
+
+    h = h<10 ? "0" + h: h;
+    m= m<10 ? "0"+m :m;
+    s= s<10? "0"+s :s;
+
+    hourE.innerText= h;
+    minuteE.innerHTML= m;
+    secondE.innerHTML=s;
+    ampmE.innerText= ampm;
+    setTimeout(()=>{
+     upclock();
+    }, 1000)
+}
+
+upclock();
